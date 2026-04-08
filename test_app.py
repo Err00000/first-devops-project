ee46e0dc1e58d758090ffb18b84d4889be62d4bb
Err00000 <nightwish746@yahoo.com>
2026-04-08 22:56:57 +0300

    revert la test_app.py v2

--- test_app.py
+++ test_app.py
@@ -2,16 +2,10 @@ import pytest
 from app import app
 
 @pytest.fixture()
-def app():
-    app = app()
-    app.config.update({
-        "TESTING": True,
-        })
-    yield app
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 def test_hello_world(client):
     response = client.get('/')
