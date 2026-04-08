bc79391261d97c2ce21cec1c882f64084d628604
Err00000 <nightwish746@yahoo.com>
2026-04-08 22:43:52 +0300

    Revert "reparat test_app.py v2"
    This reverts commit df8b115cef9281cc7fe6a022ebb73c6c859542ef.

--- test_app.py
+++ test_app.py
@@ -2,10 +2,16 @@ import pytest
 from app import app
 
 @pytest.fixture()
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+def app():
+    app = app()
+    app.config.update({
+        "TESTING": True,
+        })
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
 
 def test_hello_world(client):
     response = client.get('/')
